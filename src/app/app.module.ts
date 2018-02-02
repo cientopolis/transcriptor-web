cd295315ec00f4cd58892b2c3331998dc891c277
cbracco <bracco66.cb@gmail.com>
2018-02-01 21:55:31 -0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { MzNavbarModule } from 'ng2-materialize';
+import { MzParallaxModule } from 'ng2-materialize';
+import { MzCardModule } from 'ng2-materialize'
+
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './modules/router/app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MzNavbarModule,
+	MzParallaxModule,
+	MzCardModule,
+	AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
