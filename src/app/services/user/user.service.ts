083397b4ef94f9c527ec0a89873064349dc390b5
Lucas <lucascuevas@live.com.ar>
2018-04-01 19:06:00 -0300

    Create account

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -5,11 +5,17 @@ import { HttpService } from '../../services/http/http.service';
 @Injectable()
 export class UserService {
   private editPath = '/api/registration';
+  private createPath = '/api/registration';
   constructor(private httpService: HttpService) { }
 
   edit(user, options = {}) {
-    console.log(user);
     return this.httpService.put([this.editPath,{user_id:user.id}], {user:user}, options);
+  }
+
+  create(user, options = {}) {
+    console.log("create");
+    console.log(user);
+    return this.httpService.post([this.createPath,{user_id:user.id}], {user:user}, options);
   }
 
 }
