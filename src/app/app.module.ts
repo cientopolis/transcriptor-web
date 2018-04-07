1d6ea763683de4e2378a9dabe20f6d7417d11701
cbracco <bracco66.cb@gmail.com>
2018-04-07 20:45:14 -0300

    initial configuration of ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClient, HttpClientModule }    from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 import { MzNavbarModule } from 'ng2-materialize';
 import { MzParallaxModule } from 'ng2-materialize';
@@ -113,7 +120,13 @@ import { TranscribeService } from './services/transcribe/transcribe.service';
     NgxEditorModule,
     AppRoutingModule,
     HttpClientModule,
-    MzTabModule
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     SimpleGlobal,
