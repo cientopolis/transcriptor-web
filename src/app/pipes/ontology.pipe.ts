156d1a6e36268ce7b6a54dd60e5a237046b606b2
lucascuevas <lucascuevas@live.com.ar>
2020-07-29 19:33:04 -0300

    add rdfs filter

--- src/app/pipes/ontology.pipe.ts
+++ src/app/pipes/ontology.pipe.ts
@@ -1,6 +1,7 @@
 import { SemanticUtils } from './../utils/semantic-utils';
 import { SimpleGlobal } from 'ng2-simple-global';
 import { Pipe, PipeTransform } from '@angular/core';
+import { Ontology } from 'app/models/ontology/ontology';
 
 @Pipe({
   name: 'ontology_type'
@@ -11,6 +12,7 @@ export class OntologyPipe implements PipeTransform {
   transform(value: string): any {
     if(value!=null){
       let ontologies = this.global['ontologies'];
+      this.addFixedPRefix(ontologies);
       let str = SemanticUtils.filterPrefixFromOntology(ontologies, value);
       str = SemanticUtils.filterURLFromOntology(ontologies, str);
       return str;
@@ -19,4 +21,14 @@ export class OntologyPipe implements PipeTransform {
     }
   }
 
+  private addFixedPRefix(ontologies){
+    let ontologyrdf = new Ontology();
+    ontologyrdf.prefix ='rdf';
+    ontologyrdf.url ='http://www.w3.org/1999/02/22-rdf-syntax-ns#';
+    ontologies.push(ontologyrdf);
+    let ontologyrdfs = new Ontology();
+    ontologyrdfs.prefix = 'rdfs';
+    ontologyrdfs.url = 'http://www.w3.org/2000/01/rdf-schema#';
+    ontologies.push(ontologyrdfs);
+  }
 }
